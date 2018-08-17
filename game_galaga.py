c6ffc24a4a8521296141c5d16fda04435c11f9ce
YoonKiSeok <yks5313@gmail.com>
2018-08-17 17:36:54 +0900

    enemy 구현 중..

--- game_galaga.py
+++ game_galaga.py
@@ -45,15 +45,31 @@ class Ship:
         # print(self.x, self.y)
         Bullet(self.x, 550, self.canvas)
 
-
 class Enemy():
+    moveVal = 5;
+    moveWayRight = True;
+
     def __init__(self, canvas, x, y):
         self.canvas = canvas;
         self.x = x;
         self.y = y;
-        self.me = self.canvas.create_image(self.x, self.y, image=PhotoImage(file="image/bullet.png"))
-        pass;
+        self.me = self.canvas.create_image(self.x, self.y, image=PhotoImage(file="image/enermy1.png"))
 
+    def move(self):
+        # 600 is maximum width I guess..
+        if(self.moveWayRight) :
+            # move right
+            self.canvas.move(self.me, self.moveVal, 0)
+            self.x += self.moveVal;
+        else :
+            # move left
+            self.canvas.move(self.me, - self.moveVal, 0)
+            self.x += self.moveVal;
+
+        # 남은 칸이 10 이하면 bullet이 사라진다.
+        # dead condition
+        #     self.canvas.delete(self.me)
+        #     Bullet.bullet_list.remove(self)
 
 class App:
     enemies = [];
@@ -63,18 +79,18 @@ class App:
     enemyArea_minY = 0;
     enemyArea_maxY = 200;
 
-    def makeEnemy(self, cnt):
+    def makeEnemy(self, canvas, cnt):
         self.enemyCount = cnt;
 
         # TODO for loop make enemy
-        self.enemies.append(Enemy(self.canvas, 50, 50));
+        #self.enemies.append(Enemy(canvas, 50, 50));
 
     def __init__(self):
         self.window = Tk()
         self.canvas = Canvas(self.window, width=800, height=600)
         self.canvas.pack()
         self.ship = Ship(self.canvas);
-        self.makeEnemy(1);
+        #self.makeEnemy(self.canvas, 1);
 
     def bind_event(self):
         self.window.bind('<Left>', self.ship.move_left)
@@ -87,7 +103,8 @@ class App:
             bullet.move()
 
         # enenmy 이동 처리
-        pass;
+        # for enemy in self.enemies:
+        #     enemy.move();
 
         self.window.after(10, self.loop)
 
